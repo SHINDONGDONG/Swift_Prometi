55fc1242f34ae29f355fdafa8b6ceadddff459ed
mincheol <homerjay@naver.com>
2022-02-24 10:48:41 +0900

    hidden keyboard

--- Swift_Prometi/Controllers/RootViewController.swift
+++ Swift_Prometi/Controllers/RootViewController.swift
@@ -44,21 +44,19 @@ class RootViewController: UIViewController {
        let tf = UITextField()
         return tf
     }()
-    
-    
+
     //MARK: Life Cycle funcions
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewComponent()
-    }
-    
+        self.hiddenKeyboard()
+     }
     
     //MARK: Configures
     
     func configureViewComponent() {
         //self는 RootViewController를 의미 Rootviewcontroller는 UIViewController를 가지고있음.
-        self.view.backgroundColor = .systemBackground
-        
+        view.backgroundColor = .systemBackground
         //view에 text를 추가시켜준다.
         view.addSubview(helloWwolrdLabel)
         //code로 constraints를 주려면 false를 해주고 제약을 걸어야한다
@@ -93,5 +91,22 @@ class RootViewController: UIViewController {
         helloWorldTextField.rightAnchor.constraint(equalTo: helloWorldView.rightAnchor, constant: -4).isActive = true
         
     }
+
+}
+
+extension UIViewController {
+    func hiddenKeyboard() {
+        //tap제스쳐를 선언 후 타겟은 self,(view) selector를 지정해준 후 특정동작을 실행시킨다.
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        //cancelsTouchesInview = true는 기존에 일어나고있는 touch 이벤트를 취소시키는 속성
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    //    아무곳이나 눌러도 keyboard가 사라지는 메서드
+        @objc func dismissKeyboard() {
+            print("clied endEditing")
+            view.endEditing(true)
+
+        }
 
 }
